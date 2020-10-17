45ba306ba85d7896d313ffa2f475e327721d9e20
Hossein Mohammadi <hosseinm.developer@gmail.com>
2020-10-17 19:08:23 +0330

    add image require

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+const logo = require("./logo192.png");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
